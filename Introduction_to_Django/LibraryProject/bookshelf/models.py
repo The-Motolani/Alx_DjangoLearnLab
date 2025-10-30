ce3068ac7be09ed050203561367ae7f5ba131cb5
Motolani <alebiosumotolani@gmail.com>
2025-10-30 23:22:12 +0100

    created Book class model

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -1,10 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Book:
-    # Constructor method to initialize attributes
-    def __init__(self, title, author, publication_year):
-        self.title = title #instance variable
-        self.author = author #instance variable 
-        self.publication_year = publication_year #instance variable
-    
+class Book(models.Model):
+        title = models.CharField(max_length=200)
+        author = models.CharField(max_length=100)
+        published_date = models.DateField()
