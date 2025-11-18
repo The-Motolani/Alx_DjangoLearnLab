a3446863fc5d4e9404d3813ba210dc8f4d4c4d73
Motolani <alebiosumotolani@gmail.com>
2025-11-18 21:47:22 +0100

    Introduction to Building APIs with Django REST Framework

---
+++ api_project/api/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import viewsets
+from rest_framework.generics import ListAPIView
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated
+
+class BookList(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
