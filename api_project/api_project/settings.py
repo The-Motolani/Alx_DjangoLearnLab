496ca10c995e329a154ec090abf46d49aabe2ff4
Motolani <alebiosumotolani@gmail.com>
2025-11-19 08:31:11 +0100

    Implementing Authentication and Permissions in Django REST Framework

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -77,6 +77,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
 }
 
 
