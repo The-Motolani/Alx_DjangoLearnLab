382d2f583e02b75d5b72e87cb2e24c6c6a1461a8
Motolani <alebiosumotolani@gmail.com>
2025-11-29 21:52:13 +0100

    Implementing Filtering, Searching, and Ordering in Django REST Framework

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
-from rest_framework import permissions, generics, filters
+from rest_framework import permissions, generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters.rest_framework import DjangoFilterBackend
-
+from django_filters import rest_framework as filters
+from rest_framework.filters import SearchFilter, OrderingFilter
 # Create your views here.
 class IsAuthenticatedOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
     pass
+
+class BookFilter(filters.FilterSet):
+    class Meta:
+        model = Book
+        fields = {
+            "title": ["exact", "icontains"],
+            "publication_year": ["exact", "gte", "lte"],
+            "author__name": ["exact", "icontains"],
+        }
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
@@ -15,13 +24,16 @@ class BookListView(generics.ListAPIView):
     permission_classes = [permissions.AllowAny]
 
     filter_backends = [
-        DjangoFilterBackend,
-        filters.SearchFilter,
-        filters.OrderingFilter
+        filters.DjangoFilterBackend,
+        SearchFilter,
+        OrderingFilter
     ]
+
+filterset_class = BookFilter
 filterset_fields = ['title', 'author', 'publication_year']
 search_fields = ['title', 'author_name']
 ordering_fields = ['title', 'publication_year']
+ordering = ['title']
 
 class BookDetailView(generics.RetrieveAPIView):
      queryset = Book.objects.all()
