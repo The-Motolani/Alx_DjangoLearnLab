7568d65596b91cfc1279d68ef0854cdd2a1896c4
Motolani <alebiosumotolani@gmail.com>
2025-11-29 21:38:25 +0100

    Building Custom Views and Generic Views in Django REST Framework

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -9,7 +9,7 @@ class Author(models.Model):
 
 class Book(models.Model):
     title = models.CharField(max_length=250)
-    publication_year = models.IntegerField
+    publication_year = models.IntegerField()
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
 
     def __str__(self):
